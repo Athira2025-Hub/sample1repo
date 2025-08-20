ac13998ac91f69462a63b4e824a52c5bd2423717
user <user@DESKTOP-RIN5TFG>
2025-08-20 22:59:28 +0530

    commit1

---
+++ Sample1/src/main/java/sample/SampleProgram.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=25;
+int b=35;
+int c= a+b;
+System.out.println(c);
+	}
+
+}
